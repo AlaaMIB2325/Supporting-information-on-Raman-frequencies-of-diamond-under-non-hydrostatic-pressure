a21bc0d3a1fca9f40716e298972a2e26a67aff96
Alaa Mohammed Idris Bakhit <44397933+AlaaMIB2325@users.noreply.github.com>
2021-09-02 11:25:56 +0200

    Update Akahamaeq.py

--- Akahamaeq.py
+++ Akahamaeq.py
@@ -1,12 +1,12 @@
-#In this file we calculated the sigma_{z} assume that it is the pressure on the sample P using Akahama's equation. Taking the range of the frequences b/w (1700-1900) were they only observed the singet mode. 
+#In this file we calculated the sigma_{z} assume that it is the pressure on the sample P_s using Akahama's equation. Taking the range of the frequences between (1700-1900), they only observe the singlet mode. 
 
 import numpy as np
 
-def Akahameaeq(w):
-    return 3141-4.157*w+1.429*10**(-3)*w*w
+def Akahameaeq(omega):
+    return 3141-4.157*omega+1.429*10**(-3)*omega*omega
 
-w=np.arange(1700,1910,10)
-for i in w:
+omega=np.arange(1700,1910,10)
+for i in omega:
     q=Akahameaeq(i)
     print(i,q)
   
