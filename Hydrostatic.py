767c9903f55944fcd58c92b9fdd3126236baf68c
Alaa Mohammed Idris Bakhit <44397933+AlaaMIB2325@users.noreply.github.com>
2021-09-02 13:04:04 +0200

    Update Hydrostatic.py

--- Hydrostatic.py
+++ Hydrostatic.py
@@ -1,5 +1,5 @@
 #Using the obtained values of the fit parameters of our proposed experssion for omega^{s,d} in case of [111]loading and [001] loading, compare it with the experimantal data and the calculated omega for the hydrostatic case fcc.
-
+#w-->omega
 
 from scipy import optimize
 
@@ -7,7 +7,7 @@ import numpy as np
 from matplotlib import rc,rcParams
 import matplotlib.pyplot as plt
 
-##w_001^d
+#w_[001]^d
 
 a0 = 1364.423069
 b0 = 2.45332
@@ -15,7 +15,7 @@ c0 = -0.00224011
 x0 = np.linspace(0, 400, 10, endpoint = True)
 y0 = ( c0* (x0 * x0)) + (b0 * x0) + a0
 
-#w_001^s
+#w_[001]^s
 
 a1 = 1364.423069
 b1 = 2.43842
@@ -23,7 +23,7 @@ c1 = -0.00216728
 x1 = np.linspace(0, 400, 10, endpoint = True)
 y1 = ( c1* (x1 * x1)) + (b1 * x1) + a1
 
-##w_111^s
+##w_[111]^s
 
 a2 = 1344.423069
 b2 = 2.28008
@@ -32,7 +32,7 @@ x2 = np.linspace(0, 400, 10, endpoint = True)
 y2 = ( c2* (x2 * x2)) + (b2 * x2) + a2
 
 
-##w_111^d
+##w_[111]^d
 a3 = 1344.423069
 b3 = 2.28248
 c3 = -0.00175277
@@ -46,7 +46,7 @@ c4 = -0.002750
 x4 = np.linspace(0, 270, 10, endpoint = True)
 y4 = ( c4* (x4 * x4)) + (b4 * x4) + a4
 
-##ourcalculatedhydro
+##ourcalculatedhydrostatiic
 a5 = 1334.423069
 b5 = 2.259
 c5 = -0.00146152
@@ -61,9 +61,7 @@ for fname in filelist:
     F=data[:,2]
     
     plt.plot(data[:,1], data[:,2], '-g',linewidth=2, label=r'$\omega_{H}$ calculated')
-
-
-plt.plot(x4, y4, '--b',linewidth=2, label='Experimental data')
+    plt.plot(x4, y4, '--b',linewidth=2, label='Experimental data')
 
 
 
