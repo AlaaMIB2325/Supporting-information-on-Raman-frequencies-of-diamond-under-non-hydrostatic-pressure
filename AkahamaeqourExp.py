a4eda6c7ce38bfb7761f1efd8ba3d87bd62e7e0d
Alaa Mohammed Idris Bakhit <44397933+AlaaMIB2325@users.noreply.github.com>
2021-09-02 11:35:36 +0200

    Update AkahamaeqourExp.py

--- AkahamaeqourExp.py
+++ AkahamaeqourExp.py
@@ -21,7 +21,7 @@ def p(x,y):
     return (2.*x+y)/3
     
 def f(x):
-    omega_s=1900 
+    omega_s=1900   
     sigma_z=401.3900
     return (1364.423069+b*p(x,y)+c*p(x,y)*p(x,y)-(a0+a1*(p(x,y)-300)+a2*(p(x,y)-300)**2)*(y-x)-(b0+b1*(p(x,y)-300)+b2*(p(x,y)-300)**2)*(x-y)*(x-y)-(c0+c1*(p(x,y)-300)+c2*(p(x,y)-300)**2)*(y-x)*(y-x)*(y-x))-omeg-s
 
