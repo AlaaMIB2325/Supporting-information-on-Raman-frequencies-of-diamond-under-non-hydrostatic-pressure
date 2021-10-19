5dc7198fb9f0a90716ccc32aff41aff30f4c4e8a
Alaa Mohammed Idris Bakhit <44397933+AlaaMIB2325@users.noreply.github.com>
2021-10-19 16:33:57 +0200

    Add files via upload

---
+++ Shearstress(111).py
@@ -0,0 +1,119 @@
+import numpy as np
+import scipy.optimize as opt
+from scipy.optimize import basinhopping
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import*
+
+############################minf(x)##################################
+
+#####singletpara########################################
+
+d0= 0.536349         
+d1= -0.000587159     
+d2= -3.91498e-07    
+f0= -2.57406e-06     
+f1= -1.88292e-08    
+f2= -2.79329e-08     
+g0= -2.23224e-07     
+g1= 3.24683e-09     
+g2= 6.3714e-11 
+##############doublet#############################
+
+a0= -1.04981         
+a1= 0.000926585      
+a2=-3.61312e-06   
+b0= 0.000152294      
+b1= -1.62126e-06    
+b2= -2.23622e-08   
+c0= -3.23643e-07    
+c1= 4.24306e-09     
+c2= 7.10848e-11   
+     
+
+    
+
+def p(x,y):
+    return (2.*x+y)/3
+    
+    
+    
+def wd(x,y):
+    #x,y=par
+    return (1344.423069+2.28008*p(x,y)-0.00174805*p(x,y)*p(x,y)-(a0+a1*(p(x,y)-300)+a2*(p(x,y)-300)**2)*(y-x)-(b0+b1*(p(x,y)-300)+b2*(p(x,y)-300)**2)*(x-y)*(x-y)-(c0+c1*(p(x,y)-300)+c2*(p(x,y)-300)**2)*(y-x)*(y-x)*(y-x))
+    
+
+def ws(x,y):
+    #x,y=par 
+    return (1344.423069+2.28248*p(x,y)--0.00175277*p(x,y)*p(x,y)-(d0+d1*(p(x,y)-300)+d2*(p(x,y)-300)**2)*(y-x)-(f0+f1*(p(x,y)-300)+f2*(p(x,y)-300)**2)*(x-y)*(x-y)-(g0+g1*(p(x,y)-300)+g2*(p(x,y)-300)**2)*(y-x)*(y-x)*(y-x))
+
+
+def func(par):
+    x,y=par
+    return (((1344.423069+2.28008*p(x,y)-0.00174805*p(x,y)*p(x,y)-(a0+a1*(p(x,y)-300)+a2*(p(x,y)-300)**2)*(y-x)-(b0+b1*(p(x,y)-300)+b2*(p(x,y)-300)**2)*(x-y)*(x-y)-(c0+c1*(p(x,y)-300)+c2*(p(x,y)-300)**2)*(y-x)*(y-x)*(y-x))-1742.296)**2+((1344.423069+2.28248*p(x,y)--0.00175277*p(x,y)*p(x,y)-(d0+d1*(p(x,y)-300)+d2*(p(x,y)-300)**2)*(y-x)-(f0+f1*(p(x,y)-300)+f2*(p(x,y)-300)**2)*(x-y)*(x-y)-(g0+g1*(p(x,y)-300)+g2*(p(x,y)-300)**2)*(y-x)*(y-x)*(y-x))-1676.747)**2)
+
+
+
+#reswd=opt.basinhopping(wd, [110,110])
+
+#resws=opt.basinhopping(ws, [110,110],niter=100, T=1.0, stepsize=0.5, minimizer_kwargs=None, take_step=None, accept_test=None, callback=None, interval=50, disp=False, niter_success=None, seed=None)
+
+res=opt.basinhopping(func, [220,220])
+		
+#print(reswd)
+#print(resws)
+#print(res)
+#print(ws(122.37,199.45),wd(122.37,199.45))
+
+
+
+
+filelist=['Shearstress(111).dat']
+for fname in filelist:
+	data=np.loadtxt(fname)
+	sr=data[:12,5]
+	sz=data[:12,6]
+	te=data[1:,11]
+	
+	tb=(sz-sr)
+	t=2*te
+	print(tb)
+	
+
+	plot(data[:12,0],tb,'-r', linewidth=2,label=r'$\tau$ calculated')
+	plot(data[:12,0],data[:12,5],'-.g',linewidth=2,label=r'$\sigma_{r}$')
+	plot(data[:12,0],data[:12,6],'--b',linewidth=2,label=r'$\sigma_{z}$')
+	plot(data[1:,10],t,'oy',linewidth=2,label=r'$\tau$ from experiment')
+	xlabel(r'$P_s$ (GPa)', fontsize='14')
+	ylabel('Stress (GPa)',fontsize='14')
+	tick_params(labelsize=12)
+	legend(bbox_to_anchor=(0.95,0.95 ), loc=1, borderaxespad=0.)
+	axes = plt.gca()
+	fontsize = 11
+	tight_layout()
+	ylim(0,250)
+	for tick in axes.xaxis.get_major_ticks():
+    		tick.label1.set_fontsize(fontsize)
+    		
+	for tick in axes.yaxis.get_major_ticks():
+    		tick.label1.set_fontsize(fontsize)
+    		
+    		
+    		
+    	
+	tick_params(labelsize=11)
+	legend(loc="upper left",prop={"size":11})
+	
+	plt.savefig('Figure-5.pdf')
+	show()
+
+
+
+	
+
+
+
+
+
+
+
+
